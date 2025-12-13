0d932799e18589b2a25a29bc54194aa4a801215f
Yilmaz Atakan Kara <stu216493@mail.uni-kiel.de>
2025-12-13 15:16:50 +0100

    renamed ApplicationTests class

--- src/test/java/app/omniOne/OmniOneApplicationTest.java
+++ src/test/java/app/omniOne/OmniOneApplicationTest.java
@@ -2,9 +2,11 @@ package app.omniOne;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class OmniOneApplicationTests {
+@ActiveProfiles("test")
+class OmniOneApplicationTest {
 
 	@Test
 	void contextLoads() {
